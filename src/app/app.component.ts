8d33a2a413669658b095507c05a6a25e0fe75eb6
David Besse <dahodprod@gmail.com>
2023-12-21 00:36:44 +0100

    'Feat: added all pokemons, search bar'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './models/mock.pokemon-list';
+import { Pokemon } from './models/pokemon';
 
 @Component({
   selector: 'app-root', // name of the component
   standalone: true, // this component can be used in other components
   imports: [CommonModule, RouterOutlet], // import other modules
-  templateUrl: './app.component.html', // path to the template
-  styleUrl: './app.component.scss' // path to the stylesheet
+  templateUrl: './templates/app.component.html', // path to the template
+  styleUrl: './templates/app.component.scss', // path to the stylesheet
 })
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon[];
 
-export class AppComponent {
-  pokemons: string[] = ['bulbasaur', 'charmander', 'squirtle']; // name of the component
+  /**
+   * Initializes the component.
+   *
+   * @return {void} No return value.
+   */
+  ngOnInit() {
+    console.log(`init AppComponent`);
+
+    // Display all pokemons by default
+    this.pokemonSelected = this.pokemonList;
+  }
+
+  /**
+   * Filters the list of Pokemons based on the given name.
+   *
+   * @param pokemonName - The name of the Pokemon to filter by.
+   * @returns An array of Pokemon objects that match the given name.
+   */
+  filterPokemons(pokemonName: string) {
+    return (this.pokemonSelected = this.pokemonList.filter((pokemon) => {
+      if (pokemon.name.toLowerCase().includes(pokemonName.toLowerCase())) {
+        return pokemon;
+      }
+      return undefined;
+    }));
+  }
 }
