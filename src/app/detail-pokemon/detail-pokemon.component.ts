b1f96f51cdf527a897cc461e1c29ef7a20534f7e
David Besse <dahodprod@gmail.com>
2024-01-04 17:41:23 +0100

    'feat: added pokemon detail page structure, added CSS rules to main style file, modified app.component (html/css), added new fonts, modified pokemon list page'

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../models/mock.pokemon-list';
+import { Pokemon } from '../models/pokemon';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-detail-pokemon',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, PokemonTypeColorPipe ],
   templateUrl: './detail-pokemon.component.html',
-  styleUrl: './detail-pokemon.component.scss'
+  styleUrl: './detail-pokemon.component.scss',
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
 
+  pokemonList: Pokemon[];
+  pokemonSelected: Pokemon|undefined;
+
+  constructor(private router: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string|null = this.router.snapshot.paramMap.get('id');
+    
+    if (pokemonId) {
+      console.log(pokemonId);
+      this.pokemonSelected = this.pokemonList.find(pokemon => pokemon.id === +pokemonId);
+    }
+  }
 }
