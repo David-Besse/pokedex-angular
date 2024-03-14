208c8965690144967c5da972146187547ad8a688
David Besse <dahodprod@gmail.com>
2024-03-14 16:37:49 +0100

    'interceptor modified, added origin'

--- src/app/credentials.interceptor.ts
+++ src/app/credentials.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
   const modifiedRequest: HttpRequest<unknown> = req.clone({
-    headers: req.headers.set('Access-Control-Allow-Credentials', 'true'),
+    headers: req.headers.set(origin, 'https://dbwd-pokedex-api.vercel.app'),
     withCredentials: true,
   });
   return next(modifiedRequest);
