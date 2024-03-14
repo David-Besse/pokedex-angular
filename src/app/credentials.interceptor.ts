10e1ca0769c425ad30f7c44b3341c482585a70c4
David Besse <dahodprod@gmail.com>
2024-03-14 15:03:11 +0100

    'vercel.json modified..'

--- src/app/credentials.interceptor.ts
+++ src/app/credentials.interceptor.ts
@@ -1,7 +1,8 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
-  const modifiedRequest = req.clone({
+  const modifiedRequest: HttpRequest<unknown> = req.clone({
+    headers: req.headers.set('Access-Control-Allow-Credentials', 'true'),
     withCredentials: true,
   });
   return next(modifiedRequest);
