c193e596828fbca954fcaad8c6278da918d96458
David Besse <dahodprod@gmail.com>
2024-04-22 16:42:13 +0200

    'interceptor: remove ACAO and added it to pokemonservice in httpoption'

--- src/app/credentials.interceptor.ts
+++ src/app/credentials.interceptor.ts
@@ -7,9 +7,6 @@ export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
 
   const modifiedRequest: HttpRequest<unknown> = req.clone({
     withCredentials: true,
-    setHeaders: {
-      'Access-Control-Allow-Origin': 'https://dbwd-pokedex.vercel.app',
-    }
   });
 
   try {
--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -27,6 +27,7 @@ export class PokemonService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://dbwd-pokedex.vercel.app',
     }),
   };
 
