7982d918867602e7f7e075fcd8494be24e68b794
David Besse <dahodprod@gmail.com>
2024-04-22 16:30:58 +0200

    'pokemon.service modified/ interceptor modified'

--- src/app/credentials.interceptor.ts
+++ src/app/credentials.interceptor.ts
@@ -7,11 +7,6 @@ export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
 
   const modifiedRequest: HttpRequest<unknown> = req.clone({
     withCredentials: true,
-    setHeaders: {
-      'Access-Control-Allow-Origin': 'https://dbwd-pokedex.vercel.app',
-      'Access-Control-Allow-Credentials': 'true',
-      'Content-Type': 'application/json',
-    },
   });
 
   try {
