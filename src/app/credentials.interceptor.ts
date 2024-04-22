ec38fa316134a1396d00744ff8bb784156460325
David Besse <dahodprod@gmail.com>
2024-04-22 16:39:53 +0200

    'interceptor: add ACAO to client URL'

--- src/app/credentials.interceptor.ts
+++ src/app/credentials.interceptor.ts
@@ -7,6 +7,9 @@ export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
 
   const modifiedRequest: HttpRequest<unknown> = req.clone({
     withCredentials: true,
+    setHeaders: {
+      'Access-Control-Allow-Origin': 'https://dbwd-pokedex.vercel.app',
+    }
   });
 
   try {
